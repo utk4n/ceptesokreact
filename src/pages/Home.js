8158516bbe15ac375b171d34cc29fe16a24e6a87
Utkan <utkanocal@gmail.com>
2022-09-16 00:24:46 +0300

    unused lines deleted

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from "../components/Header/Header";
 import StoreInfo from "../components/StoreInfo/StoreInfo";
 import Banner from "../components/BannerContent/Banner";
 import Section from "../components/Section/Section";
 import Categories from "../components/Categories/Categories";
-import Footer from "../components/Footer/Footer";
 
 const Home = () => {
   return (
